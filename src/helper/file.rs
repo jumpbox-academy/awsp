63fe38926a5765e7e471abf2bcc7230e4384af6b
Natechawin <natechawin@gmail.com>
2021-10-20 23:54:19 +0700

    refactor: add tests

--- src/helper/file.rs
+++ src/helper/file.rs
@@ -12,3 +12,22 @@ pub fn is_profile(line: &str) -> bool {
 pub fn new_profile_regex() -> Regex {
     Regex::new(r"^\[(profile )?([^\]]+)\]$").expect("Failed to compile regex")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_profile_should_return_true_when_given_line_is_appropriate_profile_line() {
+        let line = "[someProfile]";
+
+        assert_eq!(true, is_profile(line));
+    }
+
+    #[test]
+    fn is_profile_should_return_false_when_given_line_is_not_appropriate_profile() {
+        let line = "some random text]][[";
+
+        assert_eq!(false, is_profile(line));
+    }
+}
